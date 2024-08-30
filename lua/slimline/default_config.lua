8ca39ff1b6896e2fe518fb1dd8071a6b42715f15
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2024-08-30 15:29:57 +0200

    Harmonized default config with README

--- lua/slimline/default_config.lua
+++ lua/slimline/default_config.lua
@@ -1,26 +1,8 @@
-local function create_M()
-  local M = {}
-
-  M.bold = false
-  M.verbose_mode = false
-  M.style = 'bg'
-
-  M.spaces = {
-    components = ' ',
-    left = ' ',
-    right = ' ',
-  }
-
-  M.sep = {
-    hide = {
-      first = false,
-      last = false,
-    },
-    left = '',
-    right = '',
-  }
-
-  M.components = {
+local M = {
+  bold = false, -- makes primary parts and mode bold
+  verbose_mode = false, -- Mode as single letter or as a word
+  style = 'bg', -- or "fg". Whether highlights should be applied to bg or fg of components
+  components = { -- Choose components and their location
     left = {
       'mode',
       'path',
@@ -32,22 +14,33 @@ local function create_M()
       'filetype_lsp',
       'progress',
     },
-  }
-
-  M.hl = {
+  },
+  spaces = {
+    components = ' ', -- string between components
+    left = ' ', -- string at the start of the line
+    right = ' ', -- string at the end of the line
+  },
+  sep = {
+    hide = {
+      first = false, -- hides the first separator
+      last = false, -- hides the last separator
+    },
+    left = '', -- left separator of components
+    right = '', -- right separator of components
+  },
+  hl = {
     modes = {
-      normal = 'Type',
+      normal = 'Type', -- highlight base of modes
       insert = 'Function',
       pending = 'Boolean',
       visual = 'Keyword',
       command = 'String',
     },
-    base = 'Comment',
-    primary = 'Normal',
-    secondary = 'Comment',
-  }
-
-  M.icons = {
+    base = 'Comment', -- highlight of everything in in between components
+    primary = 'Normal', -- highlight of primary parts (e.g. filename)
+    secondary = 'Comment', -- highlight of secondary parts (e.g. filepath)
+  },
+  icons = {
     diagnostics = {
       ERROR = ' ',
       WARN = ' ',
@@ -59,11 +52,7 @@ local function create_M()
     },
     folder = ' ',
     lines = ' ',
-  }
-
-  return M
-end
-
-local M = create_M()
+  },
+}
 
 return M
