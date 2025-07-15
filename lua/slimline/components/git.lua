0a0dbeef5cd47b72ad9260e3e913b6a7396eecec
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-07-15 05:51:01 +0200

    feat: git - mini.diff support

--- lua/slimline/components/git.lua
+++ lua/slimline/components/git.lua
@@ -1,31 +1,54 @@
 local C = {}
 
 local slimline = require('slimline')
+local initialized = false
+
+--- @class gitstatus
+--- @field head string
+--- @field added integer
+--- @field changed integer
+--- @field removed integer
+
+--- @type gitstatus|nil
+local status = nil
+
+local function gitsigns()
+  local s = vim.b.gitsigns_status_dict
+  if not s then
+    status = nil
+    return
+  end
+  status = { head = s.head, added = s.added, removed = s.removed, changed = s.changed }
+end
+
+local function init()
+  if initialized then return end
+  slimline.au('User', 'MiniDiffUpdated', function(data)
+    local summary = vim.b[data.buf].minidiff_summary
+    status = { head = summary.source_name, added = summary.add, removed = summary.delete, changed = summary.change }
+  end)
+  initialized = true
+end
 
 ---@param opts render.options
 ---@return string
 function C.render(opts)
-  local status = vim.b.gitsigns_status_dict
-  if not status then return '' end
-  if not status.head or status.head == '' then return '' end
+  init()
+  -- gitsigns()
+  if not status or not status.head then return '' end
 
   local icons = slimline.config.configs['git'].icons
-
-  local branch = string.format('%s %s', icons.branch, status.head)
-
-  local added = status.added and status.added > 0
-  local removed = status.removed and status.removed > 0
-  local changed = status.changed and status.changed > 0
-  local modifications = added or removed or changed
+  local head = string.format('%s %s', icons.branch, status.head)
+  local modifications = status.added or status.removed or status.changed
 
   local mods = {}
   if modifications then
-    if added then table.insert(mods, string.format('%s%s', icons.added, status.added)) end
-    if changed then table.insert(mods, string.format('%s%s', icons.modified, status.changed)) end
-    if removed then table.insert(mods, string.format('%s%s', icons.removed, status.removed)) end
+    if status.added > 0 then table.insert(mods, string.format('%s%s', icons.added, status.added)) end
+    if status.changed > 0 then table.insert(mods, string.format('%s%s', icons.modified, status.changed)) end
+    if status.removed > 0 then table.insert(mods, string.format('%s%s', icons.removed, status.removed)) end
   end
   return slimline.highlights.hl_component(
-    { primary = branch, secondary = table.concat(mods, ' ') },
+    { primary = head, secondary = table.concat(mods, ' ') },
     opts.hls,
     opts.sep,
     opts.direction,
